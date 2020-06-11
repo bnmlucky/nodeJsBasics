d2c3a0ac0e5c71480e69e01cede0ad1662cd2c03
Natalia Titova <bnmlucky@gmail.com>
2020-06-11 15:46:26 -0400

    creating a get request to an external api

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 // Problem: We need a simple way to look at a user's badge count and JavaScript points
 // Solution: Use Node.js to connect to Treehouse's API to get profile information to print out
+const https = require('https');
+const username = "chalkers";
+
+//Function to print message to console
+function printMessage(username, badgeCount, points) {
+    const message = `${username} has ${badgeCount} total badges and ${points} points in JavaScript`;
+    console.log(message);
+}
+
+
 
 // Connect to the API URL (https://teamtreehouse.com/username.json)
-// Read the data
-// Parse the data
-// Print the data
+const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
+    console.log(response.statusCode);
+    // Read the data
+    // Parse the data
+    // Print the data
+});
