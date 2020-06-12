aa56a4712eea5e9f528f463c215879085e13eaba
Natalia Titova <bnmlucky@gmail.com>
2020-06-12 19:05:58 -0400

    created a function that makes it possible to add command line arguments (the username)

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // Problem: We need a simple way to look at a user's badge count and JavaScript points
 // Solution: Use Node.js to connect to Treehouse's API to get profile information to print out
 const https = require('https');
-const username = "chalkers";
 
 //Function to print message to console
 function printMessage(username, badgeCount, points) {
@@ -9,22 +8,26 @@ function printMessage(username, badgeCount, points) {
     console.log(message);
 }
 
+function getProfile(username) {
+    // Connect to the API URL (https://teamtreehouse.com/username.json)
+    const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
+        console.log(response.statusCode);
+        // Read the data - without the toString() it will return the buffer
+        let body = "";
+        response.on('data', data => {
+            body += data.toString();
+        });
+        response.on('end', () => {
+            // Parse the data
+            const profile = JSON.parse(body);
+            printMessage(username, profile.badges.length, profile.points.JavaScript);
+        })
+        //there is an end event and there needs to be an end handler
 
-
-// Connect to the API URL (https://teamtreehouse.com/username.json)
-const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
-    console.log(response.statusCode);
-    // Read the data - without the toString() it will return the buffer
-    let body = "";
-    response.on('data', data => {
-        body += data.toString();
+        // Print the data
     });
-    response.on('end', () => {
-        // Parse the data
-        console.log(body);
-        console.log(typeof body);
-    })
-    //there is an end event and there needs to be an end handler
+}
 
-    // Print the data
-});
+const users = process.argv.slice(2);
+
+users.forEach(getProfile);
